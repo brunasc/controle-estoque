4ebbcd339e8fd7dfbdbd1ada7b3fa776c7c1cb61
Bruna Souza Chagas <bruna.chagass@hotmail.com>
2023-12-28 13:04:29 -0300

    build: instalação e configuração do angular material.

--- src/app/modules/login/login.module.ts
+++ src/app/modules/login/login.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
-
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -11,7 +11,8 @@ import { LoginComponent } from './login.component';
   ],
   imports: [
     CommonModule,
-    LoginRoutingModule
+    LoginRoutingModule,
+    MatButtonModule
   ]
 })
 export class LoginModule { }
